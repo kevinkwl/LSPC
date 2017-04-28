09e682315a3a330cc1b53add309897d3dac63066
kevinkwl <lingkangwei.kevin@gmail.com>
2017-04-28 20:42:15 +0800

    final upload

--- utils.py
+++ utils.py
@@ -7,17 +7,6 @@ import config
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), config.LIBLINEAR_PATH))
 
-
-def plotroc(true_label, pval):
-    """
-    
-    :param true_label: 
-    :param pval: 
-    :return: 
-    """
-    pass
-
-
 def getData(posXs, negXs, posTag=1, negTag=-1):
     Nmin = len(negXs)
     Nmax = len(posXs)
@@ -50,7 +39,22 @@ def saveResult(filename, pred_label, pred_value):
 def readResult(filename):
     def splitit(line):
         s = line.split()
-        return int(s[0]), float(s[1])
-    with open(SAVED + filename, "w") as saved:
+        return int(float(s[0])), float(s[1])
+    with open(SAVED + filename, "r") as saved:
         zipped = [splitit(line) for line in saved]
         return list(zip(*zipped))
+
+def readOracle():
+    with open(config.TEST_FILE + ".svm") as of:
+        true_label = [int(float(line.split()[0])) for line in of.readlines()]
+    return true_label
+
+
+from sklearn.metrics import recall_score, precision_score, f1_score, roc_curve, accuracy_score
+
+def score(true_label, pred_label):
+    acc = accuracy_score(true_label, pred_label)
+    precision = precision_score(true_label, pred_label)
+    recall = recall_score(true_label, pred_label)
+    f1 = f1_score(true_label, pred_label)
+    return acc, precision, recall, f1
